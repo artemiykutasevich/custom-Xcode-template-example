440a9a03c40c0da7052f3d6e353ce1bd81c0d902
Artem Kutasevich <artemiy.kutasevich@gmail.com>
2024-08-13 15:36:34 +0300

    [ADD] HomeFile.swift

---
+++ CustomXcodeTemplateExample/HomeFile.swift
@@ -0,0 +1,33 @@
+//
+// 
+//  HomeFile.swift
+//  CustomXcodeTemplateExample
+//
+//  Created by Artem Kutasevich on 13.08.24.
+//
+//
+
+// MARK: - HomeProtocol
+
+protocol HomeProtocol {}
+
+// MARK: - HomeViewController
+
+final class HomeFile {
+    
+    // Properties
+    
+    init() {
+        create()
+    }
+    
+    // Functions
+    
+    func create() {
+        // do something
+    }
+}
+
+// MARK: - extension
+
+extension HomeFile: HomeProtocol {}
