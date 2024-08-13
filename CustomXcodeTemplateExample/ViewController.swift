12f4790bb1abad6029df17d0afd2aad61cb2a8d8
Artem Kutasevich <artemiy.kutasevich@gmail.com>
2024-08-13 15:02:32 +0300

    Initial Commit

---
+++ CustomXcodeTemplateExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomXcodeTemplateExample
+//
+//  Created by Artem Kutasevich on 13.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
