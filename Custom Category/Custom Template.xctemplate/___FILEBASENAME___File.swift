aa538e216a9b022711c528f87ddb2efd45cdf9ec
Artem Kutasevich <82845071+artemiykutasevich@users.noreply.github.com>
2024-08-14 21:44:36 +0300

    [ADD] example files

---
+++ Custom Category/Custom Template.xctemplate/___FILEBASENAME___File.swift
@@ -0,0 +1,28 @@
+//
+// ___FILEHEADER___
+//
+
+// MARK: - ___VARIABLE_productName:identifier___Protocol
+
+protocol ___VARIABLE_productName:identifier___Protocol {}
+
+// MARK: - ___VARIABLE_productName:identifier___ViewController
+
+final class ___FILEBASENAMEASIDENTIFIER___ {
+    
+    // Properties
+    
+    init() {
+        create()
+    }
+    
+    // Functions
+    
+    func create() {
+        // do something
+    }
+}
+
+// MARK: - extension
+
+extension ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_productName:identifier___Protocol {}
